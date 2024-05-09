7ffd216b78296178a680eaaea010282a47619bed
Kinghope007 <emmanuelhope36@gmail.com>
2024-05-09 21:20:46 +0000

    readme and a users table with coutry

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create a user table with unique attr
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
+
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,3 @@
+0. SQL script that creates uer with attributes id, email and name
+1. SQL script that create user table with is ,, email, name and country with a default
+
